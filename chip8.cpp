3cab2a889130361c2c132aa2f2f14ebea10a1b65
LibLib97 <liamdawson4751@gmail.com>
2016-10-08 15:51:33 +0100

    Fixed bugs so now compiles

--- chip8.cpp
+++ chip8.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <cstdlib>
 
 
-class Chip8()
+class Chip8
 {
 	//16 registers (each holding 2 bytes named VX
 	unsigned char V[16];
@@ -26,7 +27,7 @@ class Chip8()
 	unsigned char fontset[80];
 
 
-	Chip8::Chip8(void)
+	Chip8(void)
 	{
 	//Set all the various registers and such to default value
 	pc = 0x200;
@@ -35,6 +36,7 @@ class Chip8()
 	sp = 0;
 	delay_timer = 0;
 	sound_timer = 0;
+	
 	
 	//Reset registers and stack
 	for(int i = 0; i < 16; i++)
@@ -61,7 +63,7 @@ class Chip8()
 	}
 
 
-	chip8::cycle()
+	void cycle()
 	{
 		//Fetch
 		//Build opcode by combining 2 places in memory to form 
@@ -78,7 +80,7 @@ class Chip8()
 				{
 					case 0x0000:
 						//Clear Screen
-						for(int i = 0; i < 2048)
+						for(int i = 0; i < 2048; i++)
 						{
 							screen[i] = 0;
 						}
@@ -100,40 +102,32 @@ class Chip8()
 				break;
 			case 0x3000:
 				//Get register number, divide by 256 to get single number
-				unsigned char vx_index = (opcode & 0x0F00) / 256;
-				if(V[vx_index] == (opcode & 0x00FF)
+				if(V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
 				{	
 					//increment pc by 2 to skip next instruction
 					pc+=2;
 				}
 				break;
 			case 0x4000:
-				unsigned char vx_index = (opcode & 0x0F00) / 256;
-				if(V[vx_index] != (opcode & 0x0FF))
+				if(V[(opcode & 0x0F00) >> 8] != (opcode & 0x0FF))
 				{
 					pc+=2;
 				}
 				break;
 			case 0x5000:
-				unsigned char vx_index = (opcode & 0x0F00) / 256;
-				unsigned char vy_index = (opcode & 0x00F0) / 16;
-				if(V[vx_index] == V[vy_index])
+				if(V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
 				{
 					pc+= 2;
 				}
 				break;
 			case 0x6000:
-				unsigned char vx_index = (opcode & 0x0F00) / 256;
-				V[vx_index] = opcode & 0xFF;
+				V[(opcode & 0x0F00) >> 8] = opcode & 0xFF;
 				break;
 			case 0x7000:
-				unsigned char vx_index = (opcode & 0x0F00) / 256;
-				V[vx_index] += opcode & 0x0FF;
+				V[(opcode & 0x0F00) >> 8] += opcode & 0x0FF;
 				break;
 			case 0x9000:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				if(V[vx_index] != V[vy_index])
+				if(V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
 				{
 					pc += 2;
 				}
@@ -145,14 +139,11 @@ class Chip8()
 				pc = (opcode & 0x0FFF) + V[0];
 				break;
 			case 0xC000:
-				vx_index = (opcode & 0x0F00) / 256;
-				V[vx_index] = 0x00FF & std::rand();
+				V[(opcode & 0x0F00) >> 8] = 0x00FF & std::rand();
 				break;
 			case 0xD000:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				unsigned char x = V[vx_index];
-				unsigned char y = V[vy_index];
+				unsigned char x = V[(opcode & 0x0F00) >> 8];
+				unsigned char y = V[(opcode & 0x00F0) >> 4];
 				unsigned char height = opcode & 0xF;
 				unsigned short pixel;
 				//set carry register to 0
@@ -173,7 +164,7 @@ class Chip8()
 								V[15] = 1;
 							}
 							//NOR pixel so if alreay 1 goes to 0 and if 0 goes to 1
-							screen[x + xbit + ((y + yline) * 64)] ^= 1'
+							screen[x + xbit + ((y + yline) * 64)] ^= 1;
 						}
 					}
 				}
@@ -187,62 +178,47 @@ class Chip8()
 		switch(opcode & 0xF00F)
 		{
 			case 0x8000:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				V[vx_index] = V[vy_index];
+				V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
 				break;
 			case 0x8001:
-		 		vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				V[vx_index] = V[vx_index] | V[vy_index];
+				V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
 				break;
 			case 0x8002:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				V[vx_index] = V[vx_index] & V[vy_index];
+				V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
 				break;
 			case 0x8003:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				V[vx_index] = V[vx_index] ^ V[vy_index];
+				V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
 				break;
 			case 0x8004:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				if((V[vx_index] + V[vy_index]) > 255)
+				if((V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) >> 4]) > 255)
 				{
 					V[15] = 1;
 					//V[vx_index] += V[vy_index];
 				}
 				else
 				{
-					V[vx_index] += V[vy_index];
+					V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F) >> 4];
 					V[15] = 0;
 				}
 				break;	
 			case 0x8005:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				if((V[vx_index] - V[vy_index]) < 0)
+				if((V[(opcode & 0x0F00) >> 8] - V[(opcode & 0x00F0) >> 4]) < 0)
 				{
 					V[15] = 1;
 					//V[vx_index] -= V[vy_index];
 				}
 				else
 				{
-					V[vx_index] -= V[vy_index];
+					V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
 					V[15] = 0;
 				}
 				break;
 			case 0x8006:
-				vx_index = (opcode & 0x0F00);
-				V[15] = vx_index & (0x80 >> 7);
-				V[vx_index] >> 1;
+				V[15] = ((opcode & 0x0F00) >> 8) & (0x80 >> 7);
+				V[(opcode & 0x0F00) >> 8] >> 1;
 				break;
 			case 0x8007:
-				vx_index = (opcode & 0x0F00) / 256;
-				vy_index = (opcode & 0x00F0) / 16;
-				if((V[vy_index] - V[vx_index]) < 0)
+				if((V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8]) < 0)
 				{
 					V[15] = 0;
 					//V[vy_index] -= V[vx_index];
@@ -250,13 +226,12 @@ class Chip8()
 				else
 				{
 					V[15] = 1;
-					V[vy_index] -= V[vx_index];
+					V[(opcode & 0x00F0) >> 4] -= V[(opcode & 0x0F00) >> 8];
 				}
 				break;
 			case 0x800E:
-				vx_index = (opcode & 0x0F00) / 256;
-				V[F] = 	V[vx_index] & 0x80;
-				V[vx_index] << 1;
+				V[15] = V[(opcode & 0x0F00) >> 8] & 0x80;
+				V[(opcode & 0x0F00) >> 8] << 1;
 				break;
 								
 		}
@@ -264,61 +239,60 @@ class Chip8()
 		switch(opcode & 0xF0FF)
 		{
 			case 0xE09E:
-				vx_index = (opcode & 0x0F00) / 256;
-				if(key == V[vx_index])
+				//loop through stored keys
+				for(int i = 0; i < 16; i++)
 				{
-					pc+=2;
+					//check if key = value in register
+					if(key[i] == V[(opcode & 0xFF00) >> 8])
+					{
+						pc+=2;
+					}
 				}
 				break;
 			case 0xE0A1:
-				vx_index = (opcode & 0x0F00) / 256;
-				if( key != V[vx_index])
+				//loop throug hkeys
+				for (int i = 0; i < 16; i++)
 				{
-					pc += 2;
+					//If the key isnt stored (hence been pressed) then TRUE
+					if( key[i] != V[(opcode & 0x0F00) >> 8])
+					{
+						pc += 2;
+					}
 				}
 				break;
 			case 0xF007:
-				vx_index = (opcode & 0x0F00) / 256;
-				V[vx_index] = delay_timer;
+				V[(opcode & 0x0F00) >> 8] = delay_timer;
 				break;
 			case 0xF00A:
-				vx_index = (opcode & 0x0F00) / 256;
-				V[X] = key;
+				V[(opcode & 0x0F00) >> 8] = key[0];
 				break;
 			case 0xF015:
-				vx_index = (opcode & 0x0F00) / 256;
-				delay_timer = V[vx_index];
+				delay_timer = V[(opcode & 0x0F00) >> 8];
 				break;
 			case 0xF018:
-				vx_index = (opcode & 0x0F00) / 256;
-				sound_timer = V[vx_index];
+				sound_timer = V[(opcode & 0x0F00) >> 8];
 				break;
 			case 0xF01E:
-				vx_index = (opcode & 0x0F00) / 256;
-				I += V[vx_index];
-				break
+				I += V[(opcode & 0x0F00) >> 8];
+				break;
 			case 0x0F029:
-				vx_index = (opcode & 0x0F00) / 256;
 				//Font table is from 0x0000 - 0x0010 so the hex character alone will point to its alternative address 
 				//Going to store fonts from address 0x0010 - 0x003E so add lookup address to that
-				I = 0x0010 + memory[V[vx_index] & 0x000F];
+				I = 0x0010 + memory[V[(opcode & 0x0F00) >> 8] & 0x000F];
 				break;
 			case 0xF033:
-				vx_index = (opcode & 0x0F00) / 256;
-				memory[I] = V[vx_index] / 100;
-				memory[I + 1] = (V[vx_index] / 10) % 100;
-				memory[I + 2] = (V[vx_index] % 100) % 10;
+				memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
+				memory[I + 1] = (V[(opcode & 0x0F00) >> 8] / 10) % 100;
+				memory[I + 2] = (V[(opcode & 0x0F00) >> 8] % 100) % 10;
 				break;
 			case 0xF055:
-				vx_index = (opcode & 0x0F00) / 256; // or >> 8
-				for(int i = 0; i <= vx_index; i++)
+				for(int i = 0; i <= ((opcode & 0x0F00) >> 8); i++)
 				{
 					memory[I + i] = V[i];
 				}
 				break;
 			case 0xF065:
-				vx_index = (opcode & 0x0F00) / 256;
-				for (int i = 0; i <= vx_index; i++)
+				for (int i = 0; i <= ((opcode & 0x0F00) >> 8); i++)
 				{	
 					V[i] = memory[I + i];			
 				}
@@ -342,7 +316,7 @@ class Chip8()
 		}
 
 	}
-}
+};
 
 
 
